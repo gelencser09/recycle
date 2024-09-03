418b605ed1739066cd1c841378fc320995d5ff38
gelencser09 <gelencser09@gmail.com>
2024-09-03 20:14:38 +0200

    fix: navbar

--- src/ui/components/navbar.tsx
+++ src/ui/components/navbar.tsx
@@ -1,15 +1,25 @@
-"use client";
-
 import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/navbar";
 import { LogoutButton } from "./logout-button";
+import { getSession } from "@/lib/actions/session";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../icons/logo.svg";
 
-export function Header() {
+export async function Header() {
+  const { email } = await getSession();
   return (
-    <Navbar>
-      <NavbarBrand>Recycle</NavbarBrand>
-      <NavbarContent justify="end">
-        <LogoutButton />
-      </NavbarContent>
+    <Navbar className="">
+      <Link href="/">
+        <NavbarBrand className="flex gap-1 items-center">
+          <Image src={logo} alt="Recycle icon" className="w-7 h-7" />
+          Recycle🇩🇰
+        </NavbarBrand>
+      </Link>
+      {email ? (
+        <NavbarContent justify="end">
+          <LogoutButton />
+        </NavbarContent>
+      ) : null}
     </Navbar>
   );
 }
