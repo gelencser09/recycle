dcb49699b2d94d5fd12e0b83cca0dae89fd4d58c
gelencser09 <gelencser09@gmail.com>
2024-09-03 19:52:09 +0200

    feat: add middleware for authorization

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getSession } from "./lib/actions/session";
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+
+  if (request.nextUrl.pathname === "/") {
+    if (!session.email)
+      return NextResponse.redirect(new URL("/auth", request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith("/auth")) {
+    if (!!session.email) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+}
